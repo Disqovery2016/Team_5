81a7f3779f1c14a9086404dc044cdf42e984c41c
Sumit Kalambe <sumitkalambe800@gmail.com>
2016-11-05 11:23:46 +0530

    progressive Commit

--- data.php
+++ data.php
@@ -9,7 +9,9 @@
       die('Could not connect: ' . mysqli_connect_error());
    }
    
-   echo 'Connected successfully';
+   echo 'Connected successfully<br>';
+   echo'--------------------------------<br>';
+   
   /* 
    $sql = "CREATE DATABASE sumitkal";
 if ($conn->query($sql) === TRUE) {
@@ -32,13 +34,51 @@ if ($conn->query($sql) === TRUE) {
 }
 
 */
-mysqli_select_db( $conn,'sumitkal' );
+    mysqli_select_db( $conn,'sumitkal' );
 
-$sql = 'SELECT vehicle_no,
- Name, Validity
- FROM vehicle';
-//mysql_select_db('TUTORIALS');
-$retval = mysqli_query( $conn,$sql  );
+echo 'vehicle details<br>';
+echo'--------------------------------<br>';
+
+$sql = 'SELECT vehicle_no,name, validity FROM vehicle';
+ 
+    $retval = mysqli_query($conn, $sql );
+   
+   if(! $retval ) {
+      die('Could not get data: ' . mysqli_error());
+   }
+   
+   while($row = mysqli_fetch_array($retval, MYSQL_ASSOC)) {
+      echo "vehicle_no :{$row['vehicle_no']}  <br> ".
+         "NAME : {$row['name']} <br> ".
+         "validity : {$row['validity']} <br> ".
+         "--------------------------------<br>";
+   }
+   
+   echo "Fetched vehicle data successfully!!!!!!!!!!!!!<br><br>";
+   echo'--------------------------------<br>';
+   
+   echo 'lecense details<br>';
+   echo'--------------------------------<br>';
+
+   
+   $sq2 = 'SELECT license_no,name, exp_date FROM license';
+ 
+    $retval1 = mysqli_query($conn, $sq2 );
+   
+   if(! $retval1 ) {
+      die('Could not get data: ' . mysqli_error());
+   }
+   
+   while($row = mysqli_fetch_array($retval1, MYSQL_ASSOC)) {
+      echo "license_no :{$row['license_no']}  <br> ".
+         "NAME : {$row['name']} <br> ".
+         "exp_date : {$row['exp_date']} <br> ".
+         "--------------------------------<br>";
+   }
+           echo "Fetched license data successfully<br>";
+   echo'--------------------------------<br>';
+
+
 /*
 if(! $retval )
 {
@@ -53,5 +93,6 @@ while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))
  "--------------------------------<br>";
 }
 echo "Fetched data successfully\n"; */
-   mysqli_close($conn);
+mysqli_close($conn);
+
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,76 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+
+    <title>TRAFFY</title>
+
+<script type="text/javascript">
+function change(){
+    
+    var lno = document.getElementById("lno").value;
+    var vno = document.getElementById("vno").value;
+    //alert(uname);
+    if(lno === "")
+    {    
+        document.getElementById('u').innerHTML = 'Plese Enter lno';
+    }
+    else
+    {
+        document.getElementById('u').innerHTML = '';
+    }
+}
+function changep(){
+       var vno = document.getElementById("vno").value;
+    if(vno === "")
+    {
+        document.getElementById('p').innerHTML = 'Plese enter vno';
+    }
+    else
+    {
+        document.getElementById('p').innerHTML = '';
+    }
+}
+</script>
+</head>
+<body><center>
+        <form action="find.html" method="post">
+<table width="503" height="117" border="0">
+  <tr>
+    <td width="117">l no</td>
+    <td width="160"><input type="text" id="lno" name="lno" onblur="change()"/></td>
+    <td width="260"><label id="u"> </label></td>
+  </tr>
+  <tr>
+    <td>v no</td>
+    <td><input type="vno" name="vno" id="vno" onblur="changep()" /></td>
+    <td width="260"><label id="p"> </label></td>
+  </tr>
+   <tr>
+    <td><input type="reset"  /></td>
+    <td><input type="submit" value="submit" id="submit"  name="submit"/>
+    
+    </td>
+  </tr>
+</table>
+</form>
+</center>
+<?php  
 include("data.php");
-if(isset($_POST['submit']))
+
+if(isset($filter_input['submit']))
 {
 session_start();
-$vehicle_no = $_POST["vehicle_no"];
-$license_no = $_POST["license_no"];
+$vno = $filter_input["vno"];
+$lno = $filter_input["lno"];
 
-$sql = "select * from license,vehicle where License_no ='".$name."' and  Vehicle_no ='".$vehicle_no."' ";
-$result=mysql_query($sql);
-$count = mysqli_num_rowsows($result);
-if($count==1)
+$sql = "select * from license,vehicle where License_no ='".$lno."' and  Vehicle_no ='".$vno."' ";
+$result= mysqli_query($conn,$sql);
+$count = mysqli_num_rows($result);
+if($count===1)
 {    
      while($row=mysqi_fetch_row($result))
     {    
-                   $_SESSION['License_no'] = $_POST['Vehicle_no'];
+                   $_SESSION['License_no'] = $filter_input['Vehicle_no'];
             header("C:\wamp\www\TOW\data.php");
        }
 }
@@ -22,4 +79,11 @@ else
     header("C:\wamp\www\TOW\data.php");
 }
 }
-?>				
+
+?>  
+</body>
+</html>				
+
+
+
+
